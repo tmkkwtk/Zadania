e96f0220ca1926258bfeba7791272abdecd03c2e
Tomasz Kwiatkowski <kwiatkowskit95@gmail.com>
2024-09-04 10:46:20 +0200

    Update second time

--- Modul4_Zad_Palindromy.py
+++ Modul4_Zad_Palindromy.py
@@ -1,7 +1,11 @@
-def palindrom(wyraz):     
+import re
+
+def palindrom(wyraz):   
+    wyraz = re.sub(r'[^a-zA-Z]', '', wyraz)
+    wyraz = wyraz.lower() 
     return True if wyraz==wyraz[::-1] and wyraz.isalpha() else False
 
-print(palindrom('kajak'))
+print(palindrom('Ka j4ak3'))
 
 
     
