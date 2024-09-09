3aba14351af78f6c7a2ac64b3824c16872a75d57
Tomasz Kwiatkowski <kwiatkowskit95@gmail.com>
2024-09-09 10:53:33 +0200

    Change to new version of calculator

--- Modul4_Zad_Kalkulator.py
+++ Modul4_Zad_Kalkulator.py
@@ -1,85 +1,91 @@
-import logging
+def add(a, b, *args):
+    addv = 0
+    for x in args:
+        addv+=x
+    return a + b + addv
 
-def kalkulator():
-    logging.basicConfig(level=logging.DEBUG)
+def sub(a, b):
+    return a - b
+
+def mul(a, b, *args):
+    addv = 1
+    for x in args:
+        addv*=x
+    return a * b * addv
+
+def div(a, b):
+    return a / b
+
+
+def get_additional_valus(op):
+    if op not in ['1', '3']:
+        return []
+    args = []
     while True:
-        dzialanie = 0
-        k = 0
-        i = 0
-        x = 1
-        skladniki = []
-        wynik = 0
-        wiadomosc = ''
-        dzialanie = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie, 5 Wyjscie: '))
-        if dzialanie == 5:
+        v = input("Podaj kolejna wartosc lub naciśnij enter aby zakończyć: ")
+        if v == "":
             break
-        elif dzialanie not in range(1, 5):
-            print('Podaj poprawny numer działania!')
+        try: 
+            v = float(v)
+        except:
+            print('Podaj prawidłową liczbę!')
+            continue
+        args.append(v)
+    return args
+
+def get_data():
+    op = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+    while True:
+        a  = input("Podaj składnik 1. ")
+        try:
+            a = float(a)
+            break
+        except:
+            print('Podaj prawidłową liczbę!')
+    while True:
+        b  = input("Podaj składnik 2. ")
+        try:
+            b = float(b)
+            if op == '4' and b == 0: print('Nie można dzielić przez 0!')
+            else:
+               break
+        except:
+            print('Podaj prawidłową liczbę!')
+    args = get_additional_valus(op)
+    return str(op), float(a), float(b), args
+
+operations = {
+    '1': add,
+    '2': sub,
+    '3': mul,
+    '4': div
+}
+
+def info(op, a, b, *args):
+    if op=='1': message = 'Dodaję: '
+    elif op=='2': message = 'Odejmuję: '
+    elif op=='3': message = 'Mnożę: '
+    elif op=='4': message = 'Dzielę: '
+
+    message = message + str(a) + ' i ' + str(b)
+    for arg in args:
+        message = message + ', ' + str(arg)
+    return message
+
+    
+def main():
+    op, a, b, args = get_data()
+    result = operations[op](a, b, *args)
+    message = info(op, a, b, *args)
+    print(message)
+    print(f'Wynik to: {result}')
+    con = input("Czy chcesz kontynuować? (tak/nie)")
+    return con
+
+if __name__ == "__main__":
+    while True:
+        con = main()
+        if con.lower() == 'tak':
             continue
         else:
-            while True:
-                skladnik = input(f'Podaj składnik {x} lub = aby wyświetlić wynik: ')
-                try: 
-                    skladnik = float(skladnik)
-                except: 
-                    if skladnik == '=':
-                        break
-                    else:
-                        print('Podaj prawidłowy składnik!') 
-                        continue
-                if skladnik == '=':
-                    break
-                else:
-                    if dzialanie == 4 and skladnik == 0.0 and len(skladniki) > 0:
-                        print('Nie można dzielić przez 0!')
-                        continue
-                    else:
-                        skladniki.append(skladnik)
-                        x+=1
-                    
-            for k in range(len(skladniki)):
-                if k == len(skladniki)-2:
-                    wiadomosc = wiadomosc + f'{skladniki[k]} '
-                elif k == len(skladniki)-1:
-                    wiadomosc = wiadomosc + f'i {skladniki[k]}'
-                else:
-                    wiadomosc = wiadomosc + f'{skladniki[k]}, '
-                k+=1
-            if dzialanie == 1:
-                wiadomosc = 'Dodaję: ' + wiadomosc
-                for skladnik in skladniki:
-                    wynik = wynik + skladnik
-            elif dzialanie == 2:
-                wiadomosc = 'Odejmuję: ' + wiadomosc
-                for i in range(len(skladniki)):
-                    if i == 0:
-                        wynik = skladniki[i]
-                    else:
-                        wynik = wynik - skladniki[i]
-                    i+=1
-            elif dzialanie == 3:
-                wiadomosc = 'Mnożę: ' + wiadomosc
-                for i in range(len(skladniki)):
-                    if i == 0:
-                        wynik = skladniki[i]
-                    else:
-                        wynik = wynik * skladniki[i]
-                    i+=1
-            elif dzialanie == 4:
-                wiadomosc = 'Dzielę: ' + wiadomosc
-                for i in range(len(skladniki)):
-                    if i == 0:
-                        wynik = skladniki[i]
-                    else:
-                        wynik = wynik / skladniki[i]
-                    i+=1
-            logging.info(wiadomosc)
-            logging.info(f'Wynik to: {wynik}')
-kalkulator()
-    
-
-
-
-
-
-
+            break
